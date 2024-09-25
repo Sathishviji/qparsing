3ae69152d6435c130d85fb857b573199062d8b10
Sabarish <116443854+Sabarish0206@users.noreply.github.com>
2024-09-25 21:45:33 +0530

    Created CO table

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 const pool = new Pool({
     connectionString: 'postgresql://postgres.xtwhfksurddqencrmhtl:Student@info12@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres',
     ssl: {
-        rejectUnauthorized: false // Required for Heroku
+        rejectUnauthorized: false 
     }
    
 })
